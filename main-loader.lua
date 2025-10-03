4f434a6baf1451240f3db4a356b56e6c8cd3288b
tykva <markdream14.gg@gmail.com>
2025-10-03 11:40:37 +0800

    Add files via upload

---
+++ main-loader.lua
@@ -0,0 +1,15 @@
+if not game:IsLoaded() then
+	game.Loaded:Wait()
+end
+
+task.wait( math.random() )
+
+local games = {
+	[35734714] = 'https://raw.githubusercontent.com/tykva666/FFFF-HUB/refs/heads/main/35734714.lua', -- AT
+    [34873522] = 'https://raw.githubusercontent.com/tykva666/FFFF-HUB/refs/heads/main/34873522.lua', -- AE
+    [34869880] = 'https://raw.githubusercontent.com/tykva666/FFFF-HUB/refs/heads/main/34869880.lua' -- PvsB
+}
+
+if games[game.CreatorId] then
+    loadstring(game:HttpGet(games[game.CreatorId]))()
+end
