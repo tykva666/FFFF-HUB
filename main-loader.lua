c05e0608e8901027549b9edbcc3b920c74fa8d87
tykva <markdream14.gg@gmail.com>
2025-10-17 12:16:58 +0800

    Update main-loader.lua

--- main-loader.lua
+++ main-loader.lua
@@ -7,7 +7,8 @@ task.wait( math.random() )
 local games = {
 	[35734714] = 'https://raw.githubusercontent.com/tykva666/FFFF-HUB/refs/heads/main/35734714.lua', -- AT
     [34873522] = 'https://raw.githubusercontent.com/tykva666/FFFF-HUB/refs/heads/main/34873522.lua', -- AE
-    [34869880] = 'https://raw.githubusercontent.com/tykva666/FFFF-HUB/refs/heads/main/34869880.lua' -- PvB
+    [34869880] = 'https://raw.githubusercontent.com/tykva666/FFFF-HUB/refs/heads/main/34869880.lua', -- PvB
+    [36051520] = 'https://raw.githubusercontent.com/tykva666/FFFF-HUB/refs/heads/main/36051520.lua' --BAB
 }
 
 if games[game.CreatorId] then
