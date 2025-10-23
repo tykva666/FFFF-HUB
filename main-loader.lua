3e59e279abd60e27d13d593455078391b521336f
tykva <markdream14.gg@gmail.com>
2025-10-23 21:34:00 +0800

    Update main-loader.lua

--- main-loader.lua
+++ main-loader.lua
@@ -8,7 +8,8 @@ local games = {
 	[35734714] = 'https://raw.githubusercontent.com/tykva666/FFFF-HUB/refs/heads/main/35734714.lua', -- AT
     [34873522] = 'https://raw.githubusercontent.com/tykva666/FFFF-HUB/refs/heads/main/34873522.lua', -- AE
     [34869880] = 'https://raw.githubusercontent.com/tykva666/FFFF-HUB/refs/heads/main/34869880.lua', -- PvB
-    [36051520] = 'https://raw.githubusercontent.com/tykva666/FFFF-HUB/refs/heads/main/36051520.lua' --BAB
+    [36051520] = 'https://raw.githubusercontent.com/tykva666/FFFF-HUB/refs/heads/main/36051520.lua', -- BAB
+    [35595896] = 'https://raw.githubusercontent.com/tykva666/FFFF-HUB/refs/heads/main/35595896.lua' -- BUB
 }
 
 if games[game.CreatorId] then
